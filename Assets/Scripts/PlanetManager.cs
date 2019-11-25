16f550357edfefdaa9d477f3de591e8783793fc6
ekarlson238 <ekarlson238@gmail.com>
2019-11-25 09:43:03 -0600

    planet manager

---
+++ Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    private Planet myPlanet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPlanet = GetComponentInChildren<Planet>();
+
+        myPlanet.GeneratePlanet();
+    }
+}
